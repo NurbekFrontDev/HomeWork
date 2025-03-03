80552b97916b661a7b9839ae18768b78332904ff
Nurbek <p896566@gmail.com>
2025-03-03 17:00:49 +0500

    Условия 🛅 homework

--- test-script.js
+++ test-script.js
@@ -1,56 +1,109 @@
-//Вариант превый:
-const a = 2;
+const num = 3;
 
-console.log(a * a + a);
-
-//Вариант второй:
-console.log(2 * 2 + 2);
-
-//Вариант третьий:
-const result = 2 * 2 + 2;
-
-console.log(result);
-
-
-const sin54 = Math.sin(54);
-const cos16 = Math.cos(16);
-const result2 = sin54 * cos16;
-const finalResult = result2 ** 2;
-
-console.log(Math.round(finalResult));
-
-const a1 = 13.2 * 71.90;
-const a2 = Math.sqrt(a1);
-const b = 7 ** 4;
-const c = 2.4 / b;
-const d = (16 * a2) / c;
-
-const leftSide = d + 3 ** Math.sqrt(49);
-const finalResult2 = leftSide * 2 ** 7;
-
-console.log(Math.round(finalResult2));
-
-
-const number1 = 25;
-const number2 = 100;
-const number3 = 939;
-
-function checkEvenOdd(num) {
-    if (num % 2 === 0) {
-        console.log(`${num} четное`)
-    } else {
-        console.log(`${num} нечетное`)
-    }
+if (num > 0) {
+  console.log("Это число положительное");
+} else if (num === 0) {
+  console.log("Это число равно нулю");
+} else if (num < 0) {
+  console.log("Это число отрицательное");
 }
 
-checkEvenOdd(number1);
-checkEvenOdd(number2);
-checkEvenOdd(number3);
+const userHeight = prompt("Введите ваш рост:");
+const userWeight = prompt("Введите ваш вес:");
 
-const isName = prompt('Как вас зовут?');
+const userWeightNum = Number(userWeight);
+const userHeightNum = Number(userHeight) / 100;
 
-if (isName) {
-    console.log(`Hello, ${isName}`);
+const calc = userWeightNum / userHeightNum ** 2;
+const calcFixed = Number(calc.toFixed(1));
+
+const yourImt = `<h1>Ваш IMT: ${calcFixed}</h1>`;
+
+if (calcFixed <= 16) {
+  document.body.innerHTML = `${yourImt} <h2>Результат: Выраженный дефицит массы тела</h2>`;
+} else if (calcFixed <= 18.5) {
+  document.body.innerHTML = `${yourImt} <h2>Результат: Недостаточная масса тела</h2>`;
+} else if (calcFixed <= 25) {
+  document.body.innerHTML = `${yourImt} <h2>Результат: Нормальная масса тела</h2>`;
+} else if (calcFixed <= 30) {
+  document.body.innerHTML = `${yourImt} <h2>Результат: Избыточная масса тела (предожирение)</h2>`;
+} else if (calcFixed <= 35) {
+  document.body.innerHTML = `${yourImt} <h2>Результат: Ожирение 1-ой степени</h2>`;
+} else if (calcFixed < 40) {
+  document.body.innerHTML = `${yourImt} <h2>Результат: Ожирение 2-ой степени</h2>`;
+} else if (calcFixed >= 40) {
+  document.body.innerHTML = `${yourImt} <h2>Результат: Ожирение 3-ой степени</h2>`;
 } else {
-    console.log('Hello, Guest!')
+  console.log("error");
 }
+
+const userMonthNum = Number(prompt("Введите число этого месяца:"));
+let MonthNum;
+
+switch (userMonthNum) {
+  case 1:
+    MonthNum = "Январь";
+    break;
+  case 2:
+    MonthNum = "Февраль";
+    break;
+  case 3:
+    MonthNum = "Март";
+    break;
+  case 4:
+    MonthNum = "Апрель";
+    break;
+  case 5:
+    MonthNum = "Май";
+    break;
+  case 6:
+    MonthNum = "Июнь";
+    break;
+  case 7:
+    MonthNum = "Июль";
+    break;
+  case 8:
+    MonthNum = "Август";
+    break;
+  case 9:
+    MonthNum = "Сентябрь";
+    break;
+  case 10:
+    MonthNum = "Октябрь";
+    break;
+  case 11:
+    MonthNum = "Ноябрь";
+    break;
+  case 12:
+    MonthNum = "Декабрь";
+    break;
+  default:
+    MonthNum = "Неверный месяц!";
+}
+
+console.log(MonthNum);
+
+const userGrade = Number(prompt("Честно напишите, как вы учились в школе?(1-5)"));
+let grade;
+
+switch (userGrade) {
+    case 1:
+        grade = 'Катастрофа! Даже Google не смог помочь... 📉';
+        break;
+    case 2:
+        grade = 'Мда... Это уже был прогресс, но учитель плакал. 😭';
+        break;
+    case 3:
+        grade = 'Ну, была троечка... Зато не двоечка! 😏'
+        break;
+    case 4:
+        grade = 'Было хорошо! Учитель был доволен, но могло быть и лучше. 😉'
+        break;
+    case 5:
+        grade = 'Гений! Ньютон был в шоке, Эйнштейн аплодировал стоя! 🚀🎓'
+        break;
+    default:
+        grade = 'Неверная оценка!'
+}
+
+console.log(grade);
